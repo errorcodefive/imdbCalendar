4e7d96c8217b52661bf56f84bafac07aa4c59416
Kyle Chin <kylechin@errorcodefive.com>
2018-07-06 10:35:12 -0400

    Working again, next step is to get it integrated with S3

--- imdb.py
+++ imdb.py
@@ -3,7 +3,7 @@ import os
 from icalendar import Calendar, Event
 import time
 import requests
-import urllib2
+
 import datetime
 import smtplib
 
@@ -20,7 +20,7 @@ class MovieInfo:
 	temp=""
 	IMDB=""
 	def outToString(self):
-		output = "Rating: "+ self.rating +"\nGenre: "+ setToString(self.genre) +"\nDescription: "+ self.desc +"\nDirector: "+self.direct+"\nActors: "+setToString(self.actors)
+		output = "Name: " + self.name + "\nRating: "+ self.rating +"\nGenre: "+ setToString(self.genre) +"\nDescription: "+ self.desc +"\nDirector: "+self.direct+"\nActors: "+setToString(self.actors)
 		return output
 	
 def setToString(set):
@@ -33,59 +33,69 @@ def setToString(set):
 today_date = time.strftime("%d")
 today_month=time.strftime("%m")
 today_year = time.strftime("%Y")
+print("Day: " + today_date)
+print("Month: " + today_month)
+print("Year: " + today_year)
 
-opener  = urllib2.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 movieClass = []
 for i in range(0,9):
-	
 	url = "http://www.imdb.com/movies-coming-soon/"+str(today_year) + "-"+str(today_month).zfill(2)+"/?ref_=cs_dt_nx"
 	today_month = int(today_month)+1
 	if int(today_month) == 13:
 		today_month = 1
 		today_year = int(today_year)+1
 		
-	
-	r = opener.open(url)
+	print ("opening URL: " + url)
+	r = requests.get(url)
+	print("Request received from URL")
 	f = open("raw_html.html", 'w')
 
-	data = r.read()
+	data = r.text
 	soup = BeautifulSoup(data, 'lxml')
-	rawHTML=soup.prettify().encode('utf8')
-	f.write(rawHTML)
+	rawHTML=soup.prettify()
 
+	f.write(rawHTML)
 	f.close
 
 
-	movieDiv = soup.div.find_all(itemtype="http://schema.org/Movie")
-	movieDiv.pop()
+	movieDiv = soup.div.find_all('div', class_='list_item', itemtype="http://schema.org/Movie")
+	print(len(movieDiv))
+	try:
+		movieDiv.pop()
+	except IndexError:
+		print("IndexError, movieDiv is empty")
 	for mov in movieDiv:
 		tempMov = MovieInfo()
 		tempMov.name = mov.find(name = "a", itemprop="url").get_text().strip()
-
+		#print("Movie Name: "+ tempMov.name)
 		if mov.find(name = "span", itemprop="contentRating"):
 			tempMov.rating=mov.find(name = "span", itemprop="contentRating").get_text().strip()
+			#print("Movie Rating: "+ tempMov.rating)
 
 		if mov.find_all(name = "span", itemprop = "genre"):
 			tempMov.genre = mov.find_all(name = "span", itemprop = "genre")
+			#print("Movie Genre: " + setToString(tempMov.genre))
 
 		if mov.find(name = "div", itemprop = "description"):
 			tempMov.desc=mov.find(name = "div", itemprop = "description").get_text().strip()
+			#print("Movie Desc: " + tempMov.desc)
 
 		if mov.find(name = "span", itemprop="director"):
 			tempMov.direct = mov.find(name = "span", itemprop="director").span.a.get_text().strip()
-
+			#print("Movie Direct: " + tempMov.direct)
 		if mov.find_all(name = "span", itemprop = "actors"):
 			tempMov.actors = mov.find_all(name = "span", itemprop = "actors")
+			#print("Movie Actors: " + setToString(tempMov.actors))
 
 		if mov.find(name = "h4", itemprop="name"):
 			if mov.find(name = "h4", itemprop="name").a['href']:
 				temp=mov.find(name = "h4", itemprop="name").a['href']
 				tempMov.IMDB = temp[7:16]
+				#print("Movie ID: " + tempMov.IMDB)
 		if tempMov.name:
 			movieClass.append(tempMov)
-			
-print "Movie Info Created"
+			print("Movie info: " + tempMov.outToString())
+print ("Movie Info Created")
 
 
 
@@ -96,32 +106,32 @@ movie_count = 0
 for m in movieClass:
 	movie_count +=1
 	getError = False
-	print "Movie Count: " + str(movie_count)
+	print ("Movie Count: " + str(movie_count))
 	url = "http://www.imdb.com/title/"+m.IMDB
-	print "Movie URL: " + url
+	print ("Movie URL: " + url)
 	try:
-		r = opener.open(url)
-		mData = r.read()
+		r = requests.get(url)
+		mData = r.text
 		mSoup = BeautifulSoup(mData, 'lxml')
-		print "Movie HTML Loaded"
+		print ("Movie HTML Loaded")
 		try:
 			m.temp = mSoup.find(name = "meta", itemprop="datePublished")['content']
 		except TypeError:
 			getError = True
-			print "Error"
+			print ("Error")
 			
 		if getError == False:
-			print m.temp
+			print ("M.temp: " + m.temp)
 			if len(m.temp)>8:
 				m.rel_year = m.temp[:4]
 				m.rel_month=m.temp[5:7]
 				m.rel_day=m.temp[8:]
-				print"Adding to calendar"
+				print ("Adding to calendar")
 				event = Event()
 				event.add('summary', m.name)
 				
-				print m.name
-				print "\n"
+				print (m.name)
+				print ("\n")
 				
 				event.add('description',m.outToString()+"Link:"+url)
 				event.add('dtstart',datetime.datetime(int(m.rel_year),int(m.rel_month),int(m.rel_day),21,0,0))
@@ -129,11 +139,11 @@ for m in movieClass:
 				event.add('dtstamp',datetime.datetime.now())
 				cal.add_component(event)
 	except URLError:
-		print "Timeout"
-f2=open('IMDB.ics','wb')
+		print ("Timeout")
+f2=open('./IMDB.ics','wb')
 f2.write(cal.to_ical())
 f2.close()
-print "done"
+print ("done")
 
-
-	
+#ToDO:
+#Write to S3
